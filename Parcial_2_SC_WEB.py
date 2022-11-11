7ea1b07b46f65d07d4dc164db25d1ec53b8b6ab5
Luisgarcav <114874130+Luisgarcav@users.noreply.github.com>
2022-11-10 19:14:58 -0600

    Update Parcial_2_SC_WEB.py

--- Parcial_2_SC_WEB.py
+++ Parcial_2_SC_WEB.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 st.header("Calcula tu capital final al paso del tiempo.")
-st.subheader("Ingrese el capital inicial: ")
-capital_inicial = st.number_input()
-st.subheader("La tasa de interes es: ")
-interes = st.number_input()
-st.subheader("Ingrese los años a transcurrir: ")
-años = st.slider()
+
+capital_inicial = st.number_input("Ingrese el capital inicial: ")
+interes = st.number_input("La tasa de interes es: ")
+años = st.slider("Ingrese los años a transcurrir: ")
 
 capital_final = float(round(capital_inicial * (1 + interes / 100) ** años, 2))
 
-st.header("El capital final es de: ", capital_final)
+st.subheader("El capital final es de: ")
+st.write(capital_final)
+
